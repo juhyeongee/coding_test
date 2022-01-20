baebe23746b800e213002dbefef5bd28489637a1
juhyeong <juhyeongee@gmail.com>
2022-01-20 14:43:47 +0900

    10828_solve

--- 10828_stack.py
+++ 10828_stack.py
@@ -1,37 +1,34 @@
-import re
+import sys
 
 
-def stacker():
-    stack = []
-    N = input()
-    for i in range(int(N)):
-        input_ = input()
-        push_pattern = re.compile('(\D*)(\d*)?')
-        m = push_pattern.search(input_)
-        # == m = re.match('push \D*', input_)
-        order = m.group(1)
-        push_num = m.group(2)
+stack = []
+N = int(sys.stdin.readline())
+for i in range(N):
+    input_ = sys.stdin.readline().split()
+    order = input_[0]
+    #push_pattern = re.compile('(\D*)(\d*)?')
+    #m = push_pattern.search(input_)
+    # == m = re.match('push \D*', input_)
+    #order = m.group(1)
+    #push_num = m.group(2)
+    
+    if order == "push": #띄어쓰기 아  !
+        stack.append(input_[1])
+    elif order == "top":
+        if len(stack) == 0 :
+            print(-1)
+        else:
+            print(stack[-1])
+    elif order == "empty":
+        if len(stack) == 0:
+            print(1)
+        else: 
+            print(0)
+    elif order =="size":
+        print(len(stack))
+    elif order == "pop":
+        if len(stack)  == 0:
+            print(-1)
+        else: 
+            print(stack.pop())
 
-        if order == "push ":
-            stack.append(push_num)
-        elif order == "top":
-            if len(stack) == 0 :
-                print(-1)
-            else:
-                print(stack[-1])
-        elif order == "empty":
-            if len(stack)  == 0:
-                print(1)
-            else: 
-                print(0)
-        elif order =="size":
-            print(len(stack))
-        elif order == "pop":
-            if len(stack)  == 0:
-                print(-1)
-            else: 
-                print(stack.pop())
-
-    return stack;
-
-stacker()
