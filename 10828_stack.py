a59bc0e23f5aca524e0b01d260c545277fc40067
juhyeong <juhyeongee@gmail.com>
2022-01-20 14:06:51 +0900

    10828 my first sol

--- 10828_stack.py
+++ 10828_stack.py
@@ -1,13 +1,37 @@
 import re
-stack = []
 
-input_ = input("Order me \n")
 
-push_pattern = re.compile('(\D*)(\d*)?')
-m = push_pattern.search(input_)
-# == m = re.match('push \D*', input_)
-order = m.group(1)
-push_num = m.group(2)
+def stacker():
+    stack = []
+    N = input()
+    for i in range(int(N)):
+        input_ = input()
+        push_pattern = re.compile('(\D*)(\d*)?')
+        m = push_pattern.search(input_)
+        # == m = re.match('push \D*', input_)
+        order = m.group(1)
+        push_num = m.group(2)
 
-print(order)
+        if order == "push ":
+            stack.append(push_num)
+        elif order == "top":
+            if len(stack) == 0 :
+                print(-1)
+            else:
+                print(stack[-1])
+        elif order == "empty":
+            if len(stack)  == 0:
+                print(1)
+            else: 
+                print(0)
+        elif order =="size":
+            print(len(stack))
+        elif order == "pop":
+            if len(stack)  == 0:
+                print(-1)
+            else: 
+                print(stack.pop())
 
+    return stack;
+
+stacker()
