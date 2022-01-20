3ef59da477cecb7a8cb046f71b25f2752933430a
juhyeong <juhyeongee@gmail.com>
2022-01-20 16:10:01 +0900

    9012_solve

---
+++ 9012_bracket.py
@@ -0,0 +1,26 @@
+from posixpath import split
+import sys 
+N = int(sys.stdin.readline())
+
+def check():
+    brackets = sys.stdin.readline()
+    splited_brackets = list(brackets)
+    splited_brackets.pop()
+
+    stack = []
+    try: 
+        for i in splited_brackets:
+            if i == "(":
+                stack.append(i)
+            elif i == ")":
+                stack.pop()
+
+        if len(stack) == 0:
+            print("YES")
+        else: 
+            print("NO")
+    except:
+        print("NO")
+
+for i in range(N):
+    check()
