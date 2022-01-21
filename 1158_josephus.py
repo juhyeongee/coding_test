7f619c1ab59dc33201084042b478f05396de234e
juhyeong <juhyeongee@gmail.com>
2022-01-22 02:57:47 +0900

    1158_second_Trial

--- 1158_josephus.py
+++ 1158_josephus.py
@@ -3,15 +3,25 @@ import sys
 import time 
 
 start= time.time()
-trial= 0 
-que = Queue()
+#trial= 0 
+#que = Queue()
 
 input_ = sys.stdin.readline().split()
 num_list = list(range(1, int(input_[0])+1))
-order = int(input_[1])
-
+increase = int(input_[1])
+index = 0
 result = []
+#index와 increase는 분리되었어야 했다. 이게 분리가 안되니까 계속 머릿속에서 돌았던거
+while len(num_list) != 0:
+    index =+ increase -1
+    if index >= len(num_list):
+        index = index % len(num_list)
+        
+    result.append(num_list.pop(index))
 
+print(result)
+
+""" Queue import 안하고 풀기.. queue때문에 늦어지는 것 같긴함
 for i in range(len(num_list)):
     que.put(num_list[i])
 
@@ -21,9 +31,10 @@ while len(result) != len(num_list):
         result.append(que.get())
     else:
         que.put(que.get())
+"""
 #que 리스트로 만드는 법 list(que.queue)
+#pop(n)
 
-print(result)
 
 end = time.time()
 
